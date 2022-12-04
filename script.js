7e4dad4dfc29259dae68fa7c34bde3580a36ca5b
Ikenna Richard <oguejioforrichard@gmail.com>
2022-12-04 00:45:23 -1100

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const message = document.getElementById('message');
+
+const form = document.querySelector('form')
+
+let inputElement = document.getElementById('user-email');
+
+const regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+).([a-z]+)?$/;
+
+form.addEventListener('submit', validate);
+
+
+function validate(e) {
+    
+    let input = document.getElementById('user-email').value;
+    // let inputElement = document.getElementById('user-email');
+
+    if (input == '') {
+        e.preventDefault();
+
+        message.classList.add('error_message');
+
+        inputElement.style.borderColor = 'var(--light-red)';
+
+        inputElement.classList.add('myClass')
+        message.innerText= 'Whoops! It looks like you forgot to add your email'; 
+        
+    }
+    
+    else if (!regx.test(input)) {
+        e.preventDefault();
+        message.classList.add('error_message')
+        inputElement.style.borderColor = 'var(--light-red)'
+        message.innerText = 'Please provide a valid email address'; 
+       
+    }
+ 
+}
